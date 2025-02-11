87adce656d9a15d4f9bd3bb6771c56836e087a18
Adithegood177 <nemethadamnorbert@gmail.com>
2025-02-11 08:14:41 +0100

    Add project files.

---
+++ IskolaOOP/Diak.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IskolaOOP
+{
+	internal class Diak
+	{
+		private string Nev { get; set; }
+		private string Osztaly { get; set; }
+
+		private int Id { get; set; }
+
+	}
+}
